1e1ad81d83613ad94be5fc4f8a97060950c85c42
John Flinchbaugh <john@hjsoft.com>
2020-07-27 23:08:00 -0400

    provide an "all" action

--- src/covid_warehouse/core.clj
+++ src/covid_warehouse/core.clj
@@ -27,36 +27,52 @@
 (defn file-name [& lst]
   (str/replace (str/trim (str/join " " lst)) #" " "-"))
 
+(defn load-db [con path]
+  (do
+    (create-stage! con)
+    (stage-data!
+      con
+      path)
+
+    (create-dims! con)
+    (load-dim-location! con)
+    (load-dim-date! con)
+
+    (drop-fact-day! con)
+    (create-fact-day! con)
+    (load-fact-day! con)))
+
+(defn query [con args]
+  (let [[country state county] args
+        series (map shorten-keys (dw-series con country state county))]
+    (doall (map print-day series))
+    (spit (str "output/" (file-name country state county) ".html") (report series))
+    (->>
+      (cond
+        (nil? county) (dw-sums-by-state con country state)
+        :else (dw-sums-by-county con country state county))
+      (map (comp println (partial str/join " ") vals))
+      doall)))
+
 (defn -main
   [action & args]
 
   (jdbc/with-transaction [con ds]
     (cond
       (= "load" action)
-      (do
-        (create-stage! con)
-        (stage-data!
-         con
-         (first args))
-
-        (create-dims! con)
-        (load-dim-location! con)
-        (load-dim-date! con)
-
-        (drop-fact-day! con)
-        (create-fact-day! con)
-        (load-fact-day! con))
+      (load-db con (first args))
       (= "query" action)
-      (let [[country state county] args
-            series (map shorten-keys (dw-series con country state county))]
-        (doall (map print-day series))
-        (spit (str "output/" (file-name country state county) ".html") (report series))
-        (->>
-        (cond
-          (nil? county) (dw-sums-by-state con country state)
-          :else (dw-sums-by-county con country state county))
-          (map (comp println (partial str/join " ") vals))
-           doall)))))
+      (query con args)
+      (= "all" action)
+      (do
+        (load-db con (first args))
+        (query con ["US" "California"])
+        (query con ["US" "New York"])
+        (query con ["US" "Florida"])
+        (query con ["US" "Pennsylvania"])
+        (query con ["US" "Pennsylvania" "York"])
+        (query con ["US" "Pennsylvania" "Lancaster"])
+        ))))
 
 (comment
   (-main "load" "/home/john/workspace/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports")
