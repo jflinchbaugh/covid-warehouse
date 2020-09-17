4520b78f81018196340f24c239583c1ac3ba5b36
John Flinchbaugh <john@hjsoft.com>
2020-09-17 01:05:12 -0400

    try to parallelize some queries

--- src/covid_warehouse/core.clj
+++ src/covid_warehouse/core.clj
@@ -66,13 +66,20 @@
 (defn all-places
   "list all the places we care to see"
   [con]
-  (concat
-   (map (juxt :country :state :county)
-     (distinct-counties-by-state-country con {:country "US" :state "Pennsylvania"}))
-   (map (juxt :country :state)
-     (distinct-states-by-country con {:country "US"}))
-   (map (juxt :country)
-     (distinct-countries con))))
+  (apply concat
+    (pcalls 
+      #(map (juxt :country :state :county)
+        (distinct-counties-by-state-country con {:country "US" :state "Pennsylvania"}))
+      #(map (juxt :country :state)
+        (distinct-states-by-country con {:country "US"}))
+      #(map (juxt :country)
+        (distinct-countries con)))))
+
+(comment
+
+  (take 1 (all-places ds))
+
+  )
 
 (defn copy-style []
   (io/copy (io/file (io/resource "web/style.css")) (io/file "output/style.css")))
@@ -91,8 +98,7 @@
         (load-db con (first args))
         (let [all-places (all-places con)]
           (doall
-           (for [place all-places]
-             (query con place)))
+            (pmap (partial query con) all-places))
           (spit
            "output/index.html"
            (index-file all-places)))
