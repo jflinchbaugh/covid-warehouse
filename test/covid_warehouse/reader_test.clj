8baed1f21c64488565e9ae787ff99a73a904f024
John Flinchbaugh <john@hjsoft.com>
2020-11-23 17:07:45 -0500

    test date parsing

--- test/covid_warehouse/reader_test.clj
+++ test/covid_warehouse/reader_test.clj
@@ -1,6 +1,7 @@
 (ns covid-warehouse.reader-test
   (:require [covid-warehouse.reader :as sut]
-            [clojure.test :refer :all]))
+            [clojure.test :refer :all]
+            [java-time :as t]))
 
 (deftest test-read-6
   (testing "read-6"
@@ -73,7 +74,22 @@
 (deftest test-fix-numbers
   (testing "fix-numbers converts all numbers"
     (are [in out] (= out (sut/fix-numbers in))
+      {:cases "" :deaths "" :recoveries ""} {:cases nil :deaths nil :recoveries nil}
       {:cases "1" :deaths "2" :recoveries "3"} {:cases 1 :deaths 2 :recoveries 3}
       {:cases "1.0" :deaths "2.0" :recoveries "3.0"} {:cases 1 :deaths 2 :recoveries 3}
       {:cases "1.9" :deaths "2.9" :recoveries "3.9"} {:cases 1 :deaths 2 :recoveries 3}
+      )))
+
+(deftest test-fix-date
+  (testing "fix-date"
+    (are [in out] (= out (sut/fix-date in))
+      {:date "01/02/2010"} {:date (t/local-date "2010-01-02")}
+      {:date "01/02/10"} {:date (t/local-date "2010-01-02")}
+      {:date "01/02/2010 01:01"} {:date (t/local-date "2010-01-02")}
+      {:date "01/02/10 01:01"} {:date (t/local-date "2010-01-02")}
+      {:date "2010-01-02T01:01:01"} {:date (t/local-date "2010-01-02")}
+      {:date "2010-01-02 01:01:01"} {:date (t/local-date "2010-01-02")}
+      ))
+  (testing "fix-date error"
+    (is (thrown? IllegalArgumentException (sut/fix-date {:date "12"}))
       )))
