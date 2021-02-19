ffc03f3076b4567aef857db23d342d3534aec07a
John Flinchbaugh <john@hjsoft.com>
2021-02-18 19:26:41 -0500

    mem settings

--- project.clj
+++ project.clj
@@ -14,5 +14,5 @@
                  [cheshire "5.10.0"]]
   :main ^:skip-aot covid-warehouse.core
   :target-path "target/%s"
-  :jvm-opts ["-Xmx2500m" "-server"]
+  :jvm-opts ["-Xmx2048m" "-server"]
   :profiles {:uberjar {:aot :all}})
