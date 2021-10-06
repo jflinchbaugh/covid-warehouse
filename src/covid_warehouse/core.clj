d591070d479221a8da6e31d3faf7ca9b2f43c76b
John Flinchbaugh <john@hjsoft.com>
2021-10-06 00:45:11 -0400

    take output dir as param

--- src/covid_warehouse/core.clj
+++ src/covid_warehouse/core.clj
@@ -69,15 +69,15 @@
               :case-change-history (int (mean cases))
               :recovery-change-history (int (mean recoveries))}))))))
 
-(defn query [con args]
+(defn query [con dest args]
   (timer (str "query " args)
          (let [[country state county] args
                series (roll-history 7 (map shorten-keys (dw-series con country state county)))]
            (spit
-            (str "output/" (html-file-name (file-name country state county)))
+            (str dest "/" (html-file-name (file-name country state county)))
             (report series))
            (spit
-            (str "output/" (json-file-name (file-name country state county)))
+            (str dest "/" (json-file-name (file-name country state county)))
             (report-json series)))))
 
 (defn all-places
@@ -117,29 +117,29 @@
 (defn copy-file [src dest]
   (io/copy (io/file (io/resource src)) (io/file dest)))
 
-(defn copy-resources []
-  (copy-file "web/.htaccess" "output/.htaccess")
-  (copy-file "web/style.css" "output/style.css"))
+(defn copy-resources [dest]
+  (copy-file "web/.htaccess" (str dest "/.htaccess"))
+  (copy-file "web/style.css" (str dest "/style.css")))
 
-(defn publish-all [ds]
+(defn publish-all [ds dest]
   (let [all-places (all-places ds)]
     (timer "all reports"
            (doall
-            (pmap (partial query ds) all-places)))
+             (pmap (partial query ds dest) all-places)))
     (spit
-     "output/index.html"
-     (index-html all-places))
+      (str dest "/index.html")
+      (index-html all-places))
     (spit
-     "output/index.json"
+      (str dest "/index.json")
      (index-json all-places)))
-  (copy-resources))
+  (copy-resources dest))
 
 (defn usage-message []
   (println "
 Usage:
-lein run all <path>
+lein run all <path> <output>
 lein run load <path>
-lein query 'US' 'Pennsylvania'
+lein query <output> 'US' 'Pennsylvania'
 "))
 
 (defn counts [ds]
@@ -158,17 +158,17 @@ lein query 'US' 'Pennsylvania'
         (load-db con (first args)))
 
       "query"
-      (query ds args)
+      (query ds (first args) (rest args))
 
       "publish-all"
-      (publish-all ds)
+      (publish-all ds (first args))
 
       "all"
       (do
         (jdbc/with-transaction [con ds]
           (load-db con (first args)))
         (pp/pprint (counts ds))
-        (publish-all ds))
+        (publish-all ds (second args)))
 
       (usage-message)))
   (create-stage! ds))
@@ -178,17 +178,17 @@ lein query 'US' 'Pennsylvania'
 
   (-main "load" "test/files")
 
-  (-main "all" "/home/john/workspace/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports")
+  (-main "all" "/home/john/workspace/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports" "output")
 
-  (-main "query" "US" "Pennsylvania")
+  (-main "query" "output" "US" "Pennsylvania")
 
-  (-main "query" "US" "New York")
+  (-main "query" "output" "US" "New York")
 
-  (-main "query" "US" "Alabama")
+  (-main "query" "output" "US" "Alabama")
 
-  (-main "query" "Afghanistan")
+  (-main "query" "output" "Afghanistan")
 
-  (-main "query" "US" "Pennsylvania" "Lancaster")
+  (-main "query" "output" "US" "Pennsylvania" "Lancaster")
 
   (let [[country state county] ["US" "Pennsylvania" "Lancaster"]
         con ds
