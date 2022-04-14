e2dec6d0c426239844210a4b7d23f1eddc58d642
John Flinchbaugh <john@hjsoft.com>
2022-04-14 09:59:31 -0400

    upgrade libraries to latest

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :plugins []
-  :dependencies [[org.clojure/clojure "1.11.0"]
-                 [org.clojure/data.csv "1.0.0"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/data.csv "1.0.1"]
                  [clojure.java-time "0.3.3"]
                  [hiccup "1.0.5"]
-                 [com.github.seancorfield/next.jdbc "1.2.772"]
-                 [com.h2database/h2 "2.1.210"]
+                 [com.github.seancorfield/next.jdbc "1.2.780"]
+                 [com.h2database/h2 "2.1.212"]
                  [com.layerware/hugsql-adapter-next-jdbc "0.5.3"]
                  [com.layerware/hugsql-core "0.5.3"]
                  [cheshire "5.10.2"]
