dcfd51d58579db9a9a53775b6862e5a966b195ec
John Flinchbaugh <john@hjsoft.com>
2022-05-11 23:02:30 -0400

    set url in project

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject covid-warehouse "0.1.0-SNAPSHOT"
   :description "data warehouse exercise for analyzing covid-19 data"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/jflinchbaugh/covid-warehouse"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :plugins []
