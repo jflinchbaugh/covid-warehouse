35114a7f06a8a0f06036f5dcc269eefe324d2269
John Flinchbaugh <john@hjsoft.com>
2022-08-08 22:28:41 -0400

    spell option better

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,7 @@ export MALLOC_ARENA_MAX=2
 time -p java \
     -server \
     -XX:MaxRAMPercentage=70 -XX:MinRAMPercentage=70 \
-    --add-opens java.base/java.util.concurrent=ALL_UNNAMED \
+    --add-opens java.base/java.util.concurrent=ALL-UNNAMED \
     -jar app.jar \
     all \
     /data/in/csse_covid_19_data/csse_covid_19_daily_reports \
