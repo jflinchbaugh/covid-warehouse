768947c228dc7aa09e60d1ca8b72536704414db8
John Flinchbaugh <john@hjsoft.com>
2022-10-02 10:44:56 -0400

    upgrade java-time. no more `abs` warnings

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/data.csv "1.0.1"]
                  [com.taoensso/timbre "5.2.1"]
-                 [clojure.java-time "1.0.0"]
+                 [clojure.java-time "1.1.0"]
                  [hiccup "1.0.5"]
                  [cheshire "5.11.0"]
                  [org.clj-commons/digest "1.4.100"]
