648de86e1fe5f5173d149aa6f8205ca1d91a0ec7
John Flinchbaugh <john@hjsoft.com>
2023-06-21 20:52:36 -0400

    script to run

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export MALLOC_ARENA_MAX=2
+export JAVA_OPTS="-server -XX:MaxRAMPercentage=75 -XX:MinRAMPercentage=75 --add-opens java.base/java.util.concurrent=ALL-UNNAMED"
+lein run all ../COVID-19/csse_covid_19_data/csse_covid_19_daily_reports output
