9336bbaf1e503b4f78a3e0c0b361a87fb1cb6bd0
John Flinchbaugh <john@hjsoft.com>
2023-07-06 22:51:01 -0400

    more tests, real-ish data

--- test/covid_warehouse/reader_test.clj
+++ test/covid_warehouse/reader_test.clj
@@ -5,7 +5,7 @@
             [clojure.java.io :as io]))
 
 (deftest test-get-csv-files
-  (is (= ["01-01-2020.csv" "01-02-2020.csv"] (get-csv-files "test/files"))))
+  (is (= ["03-01-2020.csv" "03-01-2023.csv"] (get-csv-files "test/files"))))
 
 (deftest test-parse-date
   (testing "date formats"
@@ -19,8 +19,7 @@
       "2010-01-02 01:01" "2010-01-01"))
 
   (testing "error"
-    (is (thrown? IllegalArgumentException (parse-date "12"))
-      )))
+    (is (thrown? IllegalArgumentException (parse-date "12")))))
 
 (deftest test-overlap-location
   (is (overlap-location? {:country "US" :state "New York" :county "New York City"}))
@@ -61,3 +60,51 @@
             :state "s1"
             :county "c1"
             :date "d1"}]))))
+
+(deftest test-file->doc
+  (testing "old file"
+    (is
+     (= {:file-name "test/files/03-01-2020.csv",
+         :checksum "79529c876da6d5a109edbd5e19e7579b",
+         :places
+         '({:state "",
+           :country "Italy",
+           :date "2020-02-29",
+           :cases 1694,
+           :deaths 34,
+           :recoveries 83}
+          {:state "Yunnan",
+           :country "China",
+           :date "2020-02-29",
+           :cases 174,
+           :deaths 2,
+           :recoveries 163})}
+        (file->doc (io/file "test/files/03-01-2020.csv")))))
+
+  (testing "new file"
+    (is
+     (= {:file-name "test/files/03-01-2023.csv",
+         :checksum "9fd5127064276e79424878f674ed6132",
+         :places
+         '({:county "",
+            :state "Western Australia",
+            :country "Australia",
+            :date "2023-03-01",
+            :cases 1291077,
+            :deaths 944,
+            :recoveries nil}
+           {:county "",
+            :state "",
+            :country "Austria",
+            :date "2023-03-01",
+            :cases 5919616,
+            :deaths 21891,
+            :recoveries nil}
+           {:county "Lancaster",
+            :state "Pennsylvania",
+            :country "US",
+            :date "2023-03-01",
+            :cases 150606,
+            :deaths 2088,
+            :recoveries nil})}
+        (file->doc (io/file "test/files/03-01-2023.csv"))))))
